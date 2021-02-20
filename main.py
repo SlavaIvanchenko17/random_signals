adf1010632e60b5bd567ee67dd807dc7515e2ebd
SlavaIvanchenko17 <torstein.viking.2020@gmail.com>
2021-02-20 18:12:47 +0200

    add task

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import time as t
 import matplotlib.pyplot as plt
 import numpy as np
 
-def gen_signal(n, W_max, N):
-    signals = np.zeros(N)
+def gen_signal(n, W_max, N, struct):
+    if struct == 'ARRAY': signals = np.zeros(N)
+    elif struct == 'LIST': signals = [0] * N
     step = W_max/n
     
     for i in range(n):
@@ -15,11 +16,11 @@ def gen_signal(n, W_max, N):
             signals[t] += A * np.sin(w * t + fi)
     return signals
 
-def gen_time(n, W_max, N):
+def gen_time(n, W_max, I, struct):
     time = []
-    for i in range(N):
+    for i in range(I):
         start_time = t.time()
-        gen_signal(n, W_max, i)
+        gen_signal(n, W_max, i, struct)
         end_time = t.time()
         time.append(end_time - start_time)
     return time
@@ -42,9 +43,11 @@ n = int(input("Harmonics = "))
 W_max = int(input("Frequency = "))
 N = int(input("Number of points = "))
 I = int(input("Iterations(for time) = "))
-signal1 = gen_signal(n, W_max, N)
-signal2 = gen_signal(n, W_max, N)
-time = gen_time(n, W_max, I)
+
+signal1 = gen_signal(n, W_max, N, 'LIST')
+signal2 = gen_signal(n, W_max, N, 'LIST')
+time = gen_time(n, W_max, I, 'LIST')
+
 plt.figure(1)
 plt.plot(signal1)
 plt.title('Generate random signals')
@@ -58,6 +61,14 @@ plt.figure(4)
 plt.plot(autocorrelation(signal1))
 plt.title('Autocorrelation')
 plt.show()
+
+print("############################")
+
 print("Math expectation = " + str(math_expectation(signal1)))
 print("Dispertion = " + str(dispertion(signal1)))
 
+print("############################")
+
+yellow = "\033[93m {}\033[00m"
+print(yellow.format("Time of generate for LIST = " + str(gen_time(n, W_max, 1, 'LIST')[0]))) 
+print(yellow.format("Time of generate for ARRAY = " + str(gen_time(n, W_max, 1, 'ARRAY')[0]))) 
